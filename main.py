c9f17e08c96642380720f7599037127f22bd0f17
Canthar90 <d.cieslak90@gmail.com>
2022-12-30 14:26:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+
+load_dotenv("global.env")
+
+API_KEY = os.getenv("API_KEY")
+
+URL = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+
+response = requests.get(URL).json()
+
+
+    
+    
+st.set_page_config(
+    page_title="SPAAACE",
+    page_icon="🪐"
+)
+    
+st.title(response["title"])
+     
+st.image(response["url"])
+
+st.write(response["explanation"])
